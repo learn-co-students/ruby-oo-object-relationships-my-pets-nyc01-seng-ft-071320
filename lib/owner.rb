2252585ff8f169fd042bfe513df26c280da5df49
humorous-storage-7521 <rmwerner@umich.edu>
2020-07-16 10:18:58 -0400

    cat, dog, owner class

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,26 @@
 class Owner
-  # code goes here
+  @@all = []
+  attr_reader :name, :species
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @@all << self
+  end
+
+  def say_species
+    "I am a #{@species}."
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+    @@all.count
+  end
+
+  def self.reset_all
+    @@all = []
+  end
+
 end

8a1fdf80b7fe273b263ad39a3c0d2eb14777c9d5
Mimioj101 <mimioj@gmail.com>
2020-07-16 11:17:58 -0400

    Added Associations, buy_cat, buy_dog, sell_pets, fed_cats, walk_dogs

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,6 +1,9 @@
+# require 'pry'
+
 class Owner
   @@all = []
   attr_reader :name, :species
+
   def initialize(name)
     @name = name
     @species = "human"
@@ -22,5 +25,61 @@ class Owner
   def self.reset_all
     @@all = []
   end
+
+  def cats
+      Cat.all.select do |index|
+      index.owner == self
+    end
+  end
+
+  def dogs
+    Dog.all.select do |index|
+      index.owner == self
+    end
+  end
+
+  def buy_cat(name)
+    cat = Cat.all.find do |element|
+      element.name == name
+    end
+    cat.owner = self
+  end
+
+  # def buy_dog(name)
+  #   dog = Dog.all.find do |element|
+  #     element.name == name
+  #   end
+  #   dog.owner = self
+  # end
+
+  def walk_dogs
+    self.dogs.map do |element|
+      element.mood = "happy"
+    end
+  end
+
+  def feed_cats
+    self.cats.map do |element|
+      element.mood = "happy"
+    end
+  end
+
+  def all_pets
+    @all_pets = []
+    @all_pets << Dog.all
+    @all_pets << Cat.all
+  end
+
+  def sell_pets
+    self.dogs.map do |element|
+      element.mood = "nervous"
+      element.owner = nil
+    end
+    self.cats.map do |element|
+      element.mood = "nervous"
+      element.owner = nil
+    end
+  end
+# binding.pry
 
 end

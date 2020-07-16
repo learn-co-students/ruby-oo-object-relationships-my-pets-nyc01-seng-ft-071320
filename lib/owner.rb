7c8e2dc4f050b1527fc09ad5271e260ae0c489d2
victorscholz <vgscholz@gmail.com>
2020-07-16 10:29:38 -0400

    workin on it

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,39 @@
+require 'pry'
+
 class Owner
-  # code goes here
+  attr_reader :species
+  attr_writer
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name = nil, species)
+    @name = name
+    @species = "human"
+    @@all << self
+    # all
+  end
+
+  # def name
+  #   self.name
+  # end
+
+  def say_species
+    "I am a #{self.species}."
+  end
+
+  def count
+    @@all.count
+  end
+
+  def reset_all
+    @@all = []
+  end
+
+  def all
+    @@all
+  end
+
 end
+binding.pry
+a = Owner.new("Vincent", "human")

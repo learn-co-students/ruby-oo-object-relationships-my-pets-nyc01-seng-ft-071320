77a74c87e50849809c9565dcdf3044a77761ced0
David-Kirsch <davidjkirsch@gmail.com>
2020-07-16 12:22:54 -0400

    Completed My_Pets_OO

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,27 @@
 class Dog
+  @@all = []
+  attr_accessor :owner
+  attr_reader :name
+  def initialize(name, owner)
+    @name = name
+    @owner = owner
+    @mood = "nervous"
+    save
+  end
   # code goes here
+  def self.all
+    @@all
+  end
+
+  def save
+    @@all << self
+  end
+
+  def mood=(mood)
+    @mood = mood
+  end
+  
+  def mood 
+    @mood
+  end
 end

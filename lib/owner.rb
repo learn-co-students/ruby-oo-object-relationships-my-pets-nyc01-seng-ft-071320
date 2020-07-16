b1c37b13408908915bb2446d0cf28c916a0108b4
cdemarti <codythomasdemartin@gmail.com>
2020-07-16 10:07:08 -0400

    Class initializations

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,34 @@
 class Owner
   # code goes here
+  attr_reader :name, :species
+  
+  @@all = []
+  
+  def initialize(name, species="human")
+    @name = name 
+    @species = species 
+    save
+  end
+
+  def say_species
+    "I am a #{@species}."
+  end 
+
+  def save
+    @@all <<self
+  end
+  
+  def self.all 
+    @@all
+  end
+
+  def self.count 
+    self.all.count
+  end
+  
+  def self.reset_all 
+    @@all = []
+  end 
+
+
 end

d1dfe5445ff0d474d783d57472e92eb5085527b0
codyicastro <codyicastro@gmail.com>
2020-07-16 11:29:41 -0400

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -55,7 +55,26 @@ class Owner
     end
   end
 
+  def feed_cats
+    cats.map do |cat|
+      cat.mood = "happy"
+    end
+  end
   
+  def sell_pets
+    cats.map do |cat|
+      cat.mood = "nervous"
+      cat.owner = nil
+    end
+    dogs.map do |dog|
+      dog.mood = "nervous"
+      dog.owner = nil
+    end
+  end
+
+  def list_pets
+   "I have #{dogs.count} dog(s), and #{cats.count} cat(s)."
+  end
 
 end
 

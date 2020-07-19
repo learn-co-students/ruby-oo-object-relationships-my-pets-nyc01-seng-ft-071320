b9a13b1b7a031dd0a849d17368036bbf42e74633
stackguardian <mferdous98@aol.com>
2020-07-19 14:28:37 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,18 +1,16 @@
-class Cat
-  attr_reader :name 
+class Dog
+  attr_reader :name
   attr_accessor :owner, :mood
-  
-  @@all = []
-  def initialize(name, owner)
-    @name = name 
-    @owner = owner
-    @mood = "nervous"
-    @@all << self
-  end
-  
-  def self.all
-    @@all
-  end
-  
-  
+@@all = []
+def initialize(name, owner)
+  @name = name
+  @owner = owner
+  @mood = "nervous"
+  @@all << self 
+end
+
+def self.all
+   @@all
+end
+
 end
